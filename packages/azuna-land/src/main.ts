984779c39fa9b6a864c65cb756871cf593c1749a
Tatsuya Yamamoto <t28.is.tatsuya@gmail.com>
2020-08-07 23:38:08 +0900

    add logo font

--- packages/azuna-land/src/main.ts
+++ packages/azuna-land/src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 
+import "./assets/fonts/aoyagireisyosimo_ttf_2_01.scss";
+import "./assets/fonts/PixelMplus10-Regular.subfont.scss";
+
 Vue.config.productionTip = false;
 
 new Vue({
